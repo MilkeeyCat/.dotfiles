de0a5ffad89a4b351f82b275626e134cf93100aa
MilkeeyCat <milkeeycat@gmail.com>
2024-03-28 21:03:48 +0200

    fix: bash my bash

--- scripts/.local/scripts/low-battery-notifier.sh
+++ scripts/.local/scripts/low-battery-notifier.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 bat_files="/sys/class/power_supply/BAT0"
-bat_status=$(cat "${bat_files}/status")
-capacity=$(cat "${bat_files}/capacity")
 
-if [[ ${bat_status} == "Discharging" && ${capacity} -le 20 ]]; then
-    notify-send -u critical "ACHTUUUUUUUUUUUUNG. LOW BATTERY!!!!"
-fi
+while :
+do
+    bat_status=$(cat "${bat_files}/status")
+    capacity=$(cat "${bat_files}/capacity")
+
+    if [[ ${bat_status} == "Discharging" && ${capacity} -le 20 ]]; then
+        notify-send -u critical "ACHTUUUUUUUUUUUUNG. LOW BATTERY!!!!"
+    fi
+
+    sleep 30
+done
